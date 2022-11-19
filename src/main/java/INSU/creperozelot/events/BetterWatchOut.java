7befe82e7ae8dccb715447f9afc4792dd9767a09
creperozelot <saaarlouis@gmail.com>
2022-11-19 13:42:52 +0100

    - Inital Commit 2

--- src/main/java/INSU/creperozelot/events/BetterWatchOut.java
+++ src/main/java/INSU/creperozelot/events/BetterWatchOut.java
@@ -1,17 +1,10 @@
 package INSU.creperozelot.events;
 
-import INSU.creperozelot.utils.MYSQL;
-import INSU.creperozelot.utils.utils;
-import org.bukkit.Bukkit;
-import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
 
 public class BetterWatchOut implements CommandExecutor {
     @Override
